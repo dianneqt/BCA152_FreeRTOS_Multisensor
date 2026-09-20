66f563592d929a5cdce7cdc8a8619a1e18a0d8ad
dianneqt <princessdianne.estores@g.msuiit.edu.ph>
2026-09-20 16:46:23 +0800

    Add basic FreeRTOS diagnostic tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void taskA(void *pvParameters)
+{
+    while (true)
+    {
+        printf("Task A running\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void taskB(void *pvParameters)
+{
+    while (true)
+    {
+        printf("Task B running\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
 
 extern "C" void app_main(void)
 {
     printf("BCA152 FreeRTOS Multisensor\n");
     printf("System starting...\n");
+
+    xTaskCreate(taskA, "Task A", 2048, NULL, 1, NULL);
+    xTaskCreate(taskB, "Task B", 2048, NULL, 1, NULL);
 }
